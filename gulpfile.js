568cca660bec95c75817d1ea7e0841cc3708aa29
unknown <Jens@Christian-Pc.thorvalddata.com>
2016-03-02 11:12:52 +0100

    gulp notify

--- gulpfile.js
+++ gulpfile.js
@@ -10,9 +10,42 @@ var sourcemaps = require('gulp-sourcemaps');
 var imagemin = require('gulp-imagemin');
 var browsersync = require('browser-sync').create();
 
+var reportError = function (error) {
+    var lineNumber = (error.lineNumber) ? 'LINE ' + error.lineNumber + ' -- ' : '';
+
+    notify({
+        title: 'Task Failed [' + error.plugin + ']',
+        message: lineNumber + 'See console.',
+        sound: 'Sosumi' // See: https://github.com/mikaelbr/node-notifier#all-notification-options-with-their-defaults
+    }).write(error);
+
+    gutil.beep(); // Beep 'sosumi' again
+
+    // Inspect the error object
+    //console.log(error);
+
+    // Easy error reporting
+    //console.log(error.toString());
+
+    // Pretty error reporting
+    var report = '';
+    var chalk = gutil.colors.white.bgRed;
+
+    report += chalk('TASK:') + ' [' + error.plugin + ']\n';
+    report += chalk('PROB:') + ' ' + error.message + '\n';
+    if (error.lineNumber) { report += chalk('LINE:') + ' ' + error.lineNumber + '\n'; }
+    if (error.fileName)   { report += chalk('FILE:') + ' ' + error.fileName + '\n'; }
+    console.error(report);
+
+    // Prevent the 'watch' task from stopping
+    this.emit('end');
+}
 
 gulp.task('sass', function(){
   return gulp.src('my_project/assets/sass/i.scss')
+    .pipe(plumber({
+        errorHandler: reportError
+    }))
     //.pipe(sourcemaps.init())
       .pipe(gulpsass())
       .pipe(cssnano())
@@ -27,6 +60,9 @@ gulp.task('js', function(){
     'bower_components/bootstrap-sass/assets/javascripts/bootstrap.js',
     'my_project/assets/js/main.js'
   ])
+  .pipe(plumber({
+      errorHandler: reportError
+  }))
     //.pipe(sourcemaps.init())
       .pipe(concat('main.js'))
       .pipe(uglify())
@@ -35,9 +71,28 @@ gulp.task('js', function(){
     .pipe(browsersync.stream());
 });
 
+gulp.task('image', function(){
+    return gulp.src('my_project/assets/image/*')
+    .pipe(plumber({
+        errorHandler: reportError
+    }))
+      .pipe(imagemin({
+          optimizationlevel: 5
+      }))
+      .pipe(gulp.dest('my_project/build/image'));
+});
+
+gulp.task('browser-sync', function(){
+      browsersync.init({
+        proxy: 'C:\Users\Jens\Desktop\Project management\gulp_boilerplate\my_project'
+      });
+});
+
 gulp.task('watch', function(){
     gulp.watch('my_project/assets/sass/*.scss', ['sass']);
     gulp.watch('my_project/assets/js/*.js', ['js']);
-})
+    gulp.watch('my_project/assets/image/*', ['image']);
+    gulp.watch('my_project/**/*.html').on('change', browsersync.reload);
+});
 
-gulp.task('default', ['sass', 'js', 'watch']);
+gulp.task('default', ['sass', 'js','image', 'browser-sync', 'watch']);
